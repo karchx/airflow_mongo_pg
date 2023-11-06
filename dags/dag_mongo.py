3d8937e911806d36e7467a4f702b050034195ad9
Стив <kenethra.74@gmail.com>
2023-11-05 18:53:06 -0600

    feat: parse data with pd

--- dags/dag_mongo.py
+++ dags/dag_mongo.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.mongo.hooks.mongo import MongoHook
 from datetime import datetime
+import pandas as pd
 
 def on_failure_callback(**context):
     print(f"Task {context['task_instance_key_str']} failed.")
@@ -17,8 +18,22 @@ def get_data_mongo():
     users_collection = connection().get_collection("users")
     users = users_collection.find()
 
-    for user in users:
-        print(f"Users: {user}")
+    return list(users)
+
+
+def parse_insert_data():
+    user_data = get_data_mongo()
+
+    for user in user_data:
+        user['_id'] = str(user['_id'])
+        if 'userInformation' in user:
+            user['userInformation'] = str(user['userInformation'])
+
+    df = pd.DataFrame(list(user_data))
+
+    df.rename(columns={"_id": "_id", "userInformation": "userInformation", "username": "username", "name": "name", "lastname": "lastname", "email": "email"}, inplace=True)
+
+    print(f"Data frame: {df}")
 
 with DAG(
     dag_id="dag_load_data_mongo_v01",
@@ -31,11 +46,10 @@ with DAG(
     }
 ) as dag:
     t1 = PythonOperator(
-        task_id="get-clients",
-        python_callable=get_data_mongo,
-        op_kwargs={"result": "1"},
+        task_id="parse_insert_data",
+        python_callable=parse_insert_data,
+        #op_kwargs={"result": "1"},
         dag=dag
     )
 
     t1
-    #t1 >> t2
